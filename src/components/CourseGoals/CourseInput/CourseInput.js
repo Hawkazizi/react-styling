4b4330c00081658c1377f22228cf9f3c5d5ba116
Shahin Azizi <hawk.azizi1@gmail.com>
2022-09-25 01:08:15 +0330

    add some more features

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-sequences */
 import React, { useState } from "react";
 
 import Button from "../../UI/Button/Button";
@@ -6,18 +7,26 @@ import "./CourseInput.css";
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
 
+  const [valid, setValid] = useState(true);
+
   const goalInputChangeHandler = (event) => {
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      setValid(false);
+
+      return;
+    }
     props.onAddGoal(enteredValue);
+    setValid(true);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
+      <div className={`form-control ${!valid ? "deactive" : ""} `}>
         <label>Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
